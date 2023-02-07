dbb601a3463d157cac3a0dd14ef69ecdf3efef62
Vincenzo M <vincenzomaiorana@gmail.com>
2023-02-07 14:41:13 +0100

    added jumbotron

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,4 +1,14 @@
+import { Jumbotron, Container } from "react-bootstrap";
 const Welcome = (props) => {
-  return <></>;
+  return (
+    <>
+      <Jumbotron fluid>
+        <Container>
+          <h1>{props.title}</h1>
+          <p>{props.subtitle}</p>
+        </Container>
+      </Jumbotron>
+    </>
+  );
 };
 export default Welcome;
