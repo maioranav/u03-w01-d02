316cc0ba7dc6e2cb57eb1681a7f5891817b265e6
Vincenzo M <vincenzomaiorana@gmail.com>
2023-02-07 14:45:36 +0100

    translate fix

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <MyNav brand="EpiBooks" />
-      <Welcome title="Epicode Bookshelve" subtitle="Your Epicode Books at a glance!" />
+      <Welcome title="Epicode's Bookshelf" subtitle="Your Epicode Books at a glance!" />
       <AllTheBooks />
       <MyFooter content="Made with ❤️ by Vincenzo Maiorana" />
     </div>
