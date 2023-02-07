e7ad8e2b3b3e2eca2a0f317ca9451cc28572624d
Vincenzo M <vincenzomaiorana@gmail.com>
2023-02-07 13:26:50 +0100

    App components import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MyNav from "./components/MyNav";
+import Welcome from "./components/Welcome"
+import AllTheBooks from "./components/AllTheBooks"
+import MyFooter from "./components/MyFooter"
+
 function App() {
   return (
     <div className="App">
-
+      <MyNav brand="EpiBooks" />
+      <Welcome />
+      <AllTheBooks />
+      <MyFooter />
     </div>
   );
 }
