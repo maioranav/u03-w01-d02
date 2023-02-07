c5b7b1a935161a9dfeb8c73068b9613a0ccb5491
Vincenzo M <vincenzomaiorana@gmail.com>
2023-02-07 14:41:23 +0100

    added footer content

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -1,4 +1,13 @@
-const MyFooter = () => {
-  return <h6>Footer</h6>;
+import { Container } from "react-bootstrap";
+const MyFooter = (props) => {
+  return (
+    <>
+      <footer className="fixed-bottom bg-dark text-light">
+        <Container>
+          <h6 className="text-center mt-2">{props.content}</h6>
+        </Container>
+      </footer>
+    </>
+  );
 };
 export default MyFooter;
